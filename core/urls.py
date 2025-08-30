8b6ad0f6298bc56996585c333f6175d8d1cedba5
Jorge Agustin Loyola <agusloyola21@gmail.com>
2025-08-29 23:54:09 -0300

    web fullstack

---
+++ core/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/blog/', include('apps.blog.urls')),
+    path('api/category/', include('apps.category.urls')),
+
+
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [re_path(r"^.*", TemplateView.as_view(template_name='index.html')) ]
+# if settings.DEBUG:
+#     from django.conf.urls.static import static
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
