8b696811e10301c9ccfb034c6173f52c332212f8
Jorge Agustin Loyola <agusloyola21@gmail.com>
2025-09-26 21:53:55 -0300

    Aqui corre en local

--- core/urls.py
+++ core/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
 
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 urlpatterns += [re_path(r"^.*", TemplateView.as_view(template_name='index.html')) ]
 # if settings.DEBUG:
